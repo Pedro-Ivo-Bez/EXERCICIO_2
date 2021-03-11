8b443d5a706a6e15cee1fa539dfb16fccc205903
Pedro-Ivo-Bez <bezmarocopedroivo.contato@gmail.com>
2021-03-11 13:20:58 -0300

    Primeiro commit

---
+++ 2/2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 400;
+            int b = 200;
+
+            Console.WriteLine(a + b);
+        }
+    }
+}

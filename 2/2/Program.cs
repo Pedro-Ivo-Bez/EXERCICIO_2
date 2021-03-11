347cca67762709f72ed080bc5979a8ca9857bff2
Pedro-Ivo-Bez <bezmarocopedroivo.contato@gmail.com>
2021-03-11 13:57:57 -0300

    commit 3

--- 2/2/Program.cs
+++ 2/2/Program.cs
@@ -9,9 +9,9 @@ namespace _2
             int a = 400;
             int b = 200;
             int c = 100;
+            int d = -1;
 
-
-            Console.WriteLine(a + b * c);
+            Console.WriteLine(a + d + b * c);
         }
     }
 }
